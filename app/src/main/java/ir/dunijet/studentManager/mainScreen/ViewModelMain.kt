116db39c171c194ff9e2cc32750289efd4e8da5b
Sadegh <Sadeghkhoshbayan1@gmail.com>
2022-08-15 11:07:40 +0430

    init progress bar for loading student list

--- app/src/main/java/ir/dunijet/studentManager/mainScreen/ViewModelMain.kt
+++ app/src/main/java/ir/dunijet/studentManager/mainScreen/ViewModelMain.kt
@@ -2,14 +2,20 @@ package ir.dunijet.studentManager.mainScreen
 
 import io.reactivex.Completable
 import io.reactivex.Single
+import io.reactivex.subjects.BehaviorSubject
 import ir.dunijet.studentManager.model.MainRepository
 import ir.dunijet.studentManager.model.Student
 
 class ViewModelMain {
     private val mainRepository=MainRepository()
+    private val progressBarSubject=BehaviorSubject.create<Boolean>()
 
     fun getAllStudent():Single<List<Student>>{
+        progressBarSubject.onNext(true)
         return mainRepository.getAllStudents()
+            .doFinally {
+                progressBarSubject.onNext(false)
+            }
     }
     fun deleteStudent(id: Int):Completable{
         return mainRepository.deleteStudent(id)
